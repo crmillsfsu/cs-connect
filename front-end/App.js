ed1c96927325d3ded0a617e007c582531c26625e
sethcarney <swc20a@fsu.edu>
2023-07-14 10:56:46 -0400

    Initial Logo Component

--- front-end/App.js
+++ front-end/App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Card } from 'react-native-paper';
+import Logo from './components/Logo';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to the app</Text>
+      <Card>
+        <Logo/>
+      </Card>
       <StatusBar style="auto" />
     </View>
   );
